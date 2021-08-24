db9847f0e2f331ca81be9568281810ae45cd1aa6
dotconde <iamdeyvi@gmail.com>
2021-08-24 18:55:12 -0500

    Configure devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Setup host and port for mailer delivery in development mode.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
