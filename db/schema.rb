b1c00db1cf245356a2f0cf0eddcf56cff1367a9e
dotconde <iamdeyvi@gmail.com>
2021-08-28 13:56:36 -0500

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2021_08_26_040324) do
 
   create_table "accounts", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "organization_id", null: false
+    t.bigint "organization_id"
     t.integer "role"
     t.boolean "active"
     t.datetime "created_at", precision: 6, null: false
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_08_26_040324) do
   create_table "chats", force: :cascade do |t|
     t.jsonb "chat_data"
     t.bigint "lead_id", null: false
-    t.bigint "account_id", null: false
+    t.bigint "account_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["account_id"], name: "index_chats_on_account_id"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2021_08_26_040324) do
     t.string "name"
     t.string "email"
     t.string "phone"
-    t.bigint "stage_id", null: false
+    t.bigint "stage_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["stage_id"], name: "index_leads_on_stage_id"
