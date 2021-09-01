dbc476fc6cd23e0e2f3726a8900ebf120a13e6dc
dotconde <iamdeyvi@gmail.com>
2021-09-01 02:18:49 -0500

    Add boilerplate for chats and leads controller

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+class ChatsController < ApplicationController
+  # TODO: Destroy chat should not delete chat per se, should only delete list of messages
+
+  before_action :set_chat, only: %i[show update destroy]
+
+  # GET /chats
+  def index
+    @chats = Chat.recently_updated
+
+    render json: @chats
+  end
+
+  # GET /chats/1
+  def show
+    render json: @chat
+  end
+
+  # POST /chats
+  def create
+    @chat = Chat.new(chat_params)
+
+    if @chat.save
+      render json: @chat, status: :created, location: @chat
+    else
+      render json: @chat.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /chats/1
+  def update
+    if @chat.update(chat_params)
+      render json: @chat
+    else
+      render json: @chat.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /chats/1
+  # def destroy
+  #   @chat.destroy
+  # end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_chat
+    @chat = Chat.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def chat_params
+    params.require(:chat).permit(:lead_id, :account_id, :chat_data)
+  end
+end
