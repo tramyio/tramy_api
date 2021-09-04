dc1de9cb13529e6bf73cc7d3b34d95c3ad5e0b88
dotconde <iamdeyvi@gmail.com>
2021-09-03 22:45:16 -0500

    Add webhook that handles incoming messages

--- app/controllers/hookdeck/messages_controller.rb
+++ app/controllers/hookdeck/messages_controller.rb
@@ -3,8 +3,23 @@
 module Hookdeck
   class MessagesController < ApplicationController
     def webhook
-      puts params
+      lead = Lead.where(phone: phone_hook)
+                 .first_or_create(phone: phone_hook, name: name_hook)
+      lead.chat.chat_data['messages'] << message_hook
+      lead.chat.save
       head :ok
+    end
+
+    def phone_hook
+      params.dig(:messages, 0, :from)
+    end
+
+    def name_hook
+      params.dig(:contacts, 0, :profile, :name)
+    end
+
+    def message_hook
+      params.dig(:messages, 0).to_unsafe_h
     end
   end
 end
