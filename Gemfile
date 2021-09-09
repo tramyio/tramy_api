246d1c27015f0e60cc8bb72ca4d6783d8396c96b
dotconde <iamdeyvi@gmail.com>
2021-09-09 17:14:01 -0500

    Add setup for enviroment variables with Figaro

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'puma', '~> 4.1'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
+gem 'figaro'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,14 +269,14 @@ Devise.setup do |config|
   config.sign_out_via = :delete
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.secret = ENV['SECRET_KEY_BASE']
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
     ]
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 30.minutes.to_i
+    jwt.expiration_time = 48.hours.to_i
   end
 
   # ==> OmniAuth
