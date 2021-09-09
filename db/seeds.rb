8792b6df1176015f9e47c2c11682ac73050df7ab
dotconde <iamdeyvi@gmail.com>
2021-09-09 17:28:08 -0500

    Allow seeds temporarily for all envs

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,45 +4,45 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 
-if Rails.env.development?
-  users = [
-    { email: 'admin@tramy.io', password: 123_456 },
-    { email: 'agent1@tramy.io', password: 123_456 },
-    { email: 'agent2@tramy.io', password: 123_456 }
-  ]
+# if Rails.env.development?
+users = [
+  { email: 'admin@tramy.io', password: 123_456 },
+  { email: 'agent1@tramy.io', password: 123_456 },
+  { email: 'agent2@tramy.io', password: 123_456 }
+]
 
-  leads = [
-    { name: 'Jhon Smith', phone: '51900000001' },
-    { name: 'Alex Pretell', phone: '51999888777' },
-    { name: 'Luisa Rodriguez', phone: '51987654321' }
-  ]
+leads = [
+  { name: 'Jhon Smith', phone: '51900000001' },
+  { name: 'Alex Pretell', phone: '51999888777' },
+  { name: 'Luisa Rodriguez', phone: '51987654321' }
+]
 
-  org = Organization.create(name: 'Disoft', phone: '51999666333', provider_api_key: 'aDlgxf_sandbox')
+org = Organization.create(name: 'Disoft', phone: '51999666333', provider_api_key: 'aDlgxf_sandbox')
 
-  pipe = Pipeline.create(name: 'Clientes', organization: org)
+pipe = Pipeline.create(name: 'Clientes', organization: org)
 
-  stages = [
-    { name: 'Nuevo lead' },
-    { name: 'Llamada agendada' },
-    { name: 'Cotizacion' },
-    { name: 'Pagado' }
-  ]
+stages = [
+  { name: 'Nuevo lead' },
+  { name: 'Llamada agendada' },
+  { name: 'Cotizacion' },
+  { name: 'Pagado' }
+]
 
-  stages.each do |stage|
-    Stage.create(name: stage[:name], pipeline: pipe)
-  end
-
-  # Seeder
-  puts 'Tramy is planting seeds for users'
-  users.each do |user|
-    User.create(email: user[:email], password: user[:password])
-  end
-
-  puts 'Tramy is planting seeds for leads'
-  leads.each do |lead|
-    Lead.create(name: lead[:name], phone: lead[:phone])
-  end
-
-  puts 'Tramy is planting seeds for stages'
-  stages
+stages.each do |stage|
+  Stage.create(name: stage[:name], pipeline: pipe)
 end
+
+# Seeder
+puts 'Tramy is planting seeds for users'
+users.each do |user|
+  User.create(email: user[:email], password: user[:password])
+end
+
+puts 'Tramy is planting seeds for leads'
+leads.each do |lead|
+  Lead.create(name: lead[:name], phone: lead[:phone])
+end
+
+puts 'Tramy is planting seeds for stages'
+stages
+# end
