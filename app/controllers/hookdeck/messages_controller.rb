ee7e97b224d720e6a1f0b9ee5bd8b4ffaf2e26e2
dotconde <iamdeyvi@gmail.com>
2021-09-13 02:12:12 -0500

    Delivery messages to organization selected by number

--- app/controllers/hookdeck/messages_controller.rb
+++ app/controllers/hookdeck/messages_controller.rb
@@ -5,27 +5,35 @@ module Hookdeck
     # TODO: Check if incoming payload include messages' keys, then execute incoming webhooks logic
     # TODO: Check if incoming payload include statuses' keys, then execute status webhook logic
     skip_before_action :authenticate_user!
+    # before_action :set_organization, only: %i[webhook]
 
     def webhook
-      return if params.key?(:statuses) # Skip temporarily statuses
+      return if params.key?(:statuses) # Skip temporarily webhook statuses
 
-      lead = Lead.where(phone: phone_hook)
-                 .first_or_create(phone: phone_hook, name: name_hook)
-      lead.chat.chat_data['messages'] << message_hook
+      owner_organization = Organization.find_by(phone: organization_phone_hook)
+
+      lead = Lead.where(phone: lead_phone_hook)
+                 .first_or_create(phone: lead_phone_hook, name: lead_name_hook, organization: owner_organization)
+
+      lead.chat.chat_data['messages'] << lead_message_hook
       lead.chat.save
       head :ok
     end
 
-    def phone_hook
+    def lead_phone_hook
       params.dig(:messages, 0, :from)
     end
 
-    def name_hook
+    def lead_name_hook
       params.dig(:contacts, 0, :profile, :name)
     end
 
-    def message_hook
+    def lead_message_hook
       params.dig(:messages, 0).to_unsafe_h.reject! { |key| key == 'id' }
+    end
+
+    def organization_phone_hook
+      params[:number]
     end
   end
 end
