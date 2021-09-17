8ac3f446365a50c9575d3a560b82f8c18f14575b
dotconde <iamdeyvi@gmail.com>
2021-09-17 17:21:46 -0500

    Rename update my org info route

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,9 @@ Rails.application.routes.draw do
   resources :leads, only: %i[index show create update]
   resources :stages, only: %i[index]
   resources :pipelines, only: %i[index create update]
-  resources :organizations, only: %i[create update]
+  resources :organizations, only: %i[create]
   get 'my_organization', to: 'organizations#show'
+  patch 'my_organization', to: 'organizations#update'
 
   patch 'chats/:id/new_message', to: 'chats#new_message'  #  Chat (New message)
   get '/chats_assigned_to_me', to: 'chats#assigned_to_me' #  Chat (Assigned to me)
