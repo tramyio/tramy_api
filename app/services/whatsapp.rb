a9805d405590fc6824acbed993b9941dca1e20af
dotconde <iamdeyvi@gmail.com>
2021-09-17 01:20:30 -0500

    Fix translations for non-defined codes

--- app/services/whatsapp.rb
+++ app/services/whatsapp.rb
@@ -22,7 +22,7 @@ class Whatsapp
     when 403
       { json: I18n.t("http_response.#{whatsapp_api_response.code}.message"), status: whatsapp_api_response.code }
     else
-      { json: I18n.t("http_response.#{whatsapp_api_response.code}.message"), status: whatsapp_api_response.code }
+      { json: I18n.t('http_response.other.message'), status: whatsapp_api_response.code }
     end
   end
 
