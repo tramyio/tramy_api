90e0a5da6d9a37aae3e3e99227b2f045f1abc993
dotconde <iamdeyvi@gmail.com>
2021-09-18 02:19:30 -0500

    Add dependant destroy relation and delete non-used association

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,9 +3,8 @@
 class Organization < ApplicationRecord
   # TODO: Restrict organization to max 3 accounts
   # TODO: Encrypt provider_api_key (https://edgeguides.rubyonrails.org/active_record_encryption.html#declaration-of-encrypted-attributes)
-  has_one :setup
-  has_many :accounts
-  has_many :pipelines
+  has_many :accounts, dependent: :destroy
+  has_many :pipelines, dependent: :destroy
 
   # Fast access
   has_many :leads
