87557b303ff8a3950caa3cde477cd7858f3f7f62
dotconde <iamdeyvi@gmail.com>
2021-09-18 00:45:50 -0500

    Add security for leads

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -7,7 +7,7 @@ class Lead < ApplicationRecord
   belongs_to :stage, optional: true # For recently created lead (non-assigned)
   belongs_to :organization
 
-  scope :oldest_created, -> { order(created_at: :asc) }
+  scope :recently_created, -> { order(created_at: :desc) }
 
   validates :email, allow_nil: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :phone, presence: true, uniqueness: true
