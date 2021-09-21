373e4fd205e10ccb0a6bfff67493807f41c6a4d8
dotconde <iamdeyvi@gmail.com>
2021-09-21 15:39:07 -0500

    Return when user does not belong to a valid organization

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,6 +4,11 @@ class AccountsController < ApplicationController
   # TODO: Handle account invitation by email
   # TODO: Update account params (update organization_id)
   def index
+    if current_user.organization.nil?
+      return render json: 'This account does not belong to a valid organization',
+                    status: :not_found
+    end
+
     @accounts = Account.where(organization: current_user.organization)
     render json: AccountSerializer.new(@accounts).serializable_hash[:data]
   end
