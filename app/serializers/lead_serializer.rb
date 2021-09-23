4ecdb0c8fe69b6f5f44550a4cf52595ee7f6f6a5
dotconde <iamdeyvi@gmail.com>
2021-09-22 20:52:19 -0500

    Add date format to LeadSerializer

--- app/serializers/lead_serializer.rb
+++ app/serializers/lead_serializer.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class LeadSerializer
   include JSONAPI::Serializer
   attributes :name, :email, :phone, :stage, :created_at, :organization_id
-  attribute :stage do |object|
-    object&.stage
+
+  attribute :created_at do |object|
+    object.created_at.strftime('%d/%m/%Y %I:%M %P')
   end
 end
