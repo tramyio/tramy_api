c10f12754ec53c24c2f7d3965c3dd691d6d1757f
dotconde <iamdeyvi@gmail.com>
2021-09-27 01:52:34 -0500

    Fix for serializer in pipeline

--- app/serializers/pipeline_serializer.rb
+++ app/serializers/pipeline_serializer.rb
@@ -6,6 +6,6 @@ class PipelineSerializer
   attributes :name, :organization_id
 
   attributes :stages do |pipeline|
-    StageSerializer.new(pipeline.stages).serializable_hash[:data][:attributes]
+    StageSerializer.new(pipeline.stages).serializable_hash[:data]
   end
 end
