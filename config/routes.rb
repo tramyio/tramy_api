104d387cf631840e7a93111e6d56937a32ddd2e6
dotconde <iamdeyvi@gmail.com>
2021-10-28 19:38:35 -0500

    Include upload file route

--- config/routes.rb
+++ config/routes.rb
@@ -36,16 +36,18 @@ Rails.application.routes.draw do
   patch 'my_profile', to: 'profiles#update' # Update my profile
 
   # Chat
-  patch 'chats/:id/new_message', to: 'chats#new_message'  #  Chat (New message)
-  get '/chats_assigned_to_me', to: 'chats#assigned_to_me' #  Chat (Assigned to me)
+  patch 'chats/:id/new_message', to: 'chats#new_message'  # Chat (New message)
+  get '/chats_assigned_to_me', to: 'chats#assigned_to_me' # Chat (Assigned to me)
   get '/chats_not_assigned', to: 'chats#not_assigned' # Chat (Unassigned)
   get 'chats/:id/notes', to: 'chats#list_notes' # Chat (Get notes)
   post 'chats/:id/notes', to: 'chats#append_note' # Chat (Get notes)
   get 'templates', to: 'chats#list_templates'
+  post 'chats/:id/upload_file', to: 'chats#upload_file'
 
   # Account
   get 'accounts', to: 'accounts#index' # Team / Chat (List of agents)
 
   # Media retrieve ("proxy-like")
   get 'retrieve_media/:media_id', to: 'chats#retrieve_media'
+
 end
